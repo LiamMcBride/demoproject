788bb95cbc783a10859c2cd6d536adbd747ab1c8
liammcbride <mailmcbride56@gmail.com>
2021-10-11 15:13:46 -0400

    Updates to the admin portal

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+from django.utils import timezone
+import datetime
 
-admin.site.register(Question)
+# admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', "pub_date", "was_published_recently")
+
+    # @admin.display(
+    #     boolean = True,
+    #     ordering = 'pub_date',
+    #     description = 'Published recently?',
+    # )
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+admin.site.register(Question, QuestionAdmin)
